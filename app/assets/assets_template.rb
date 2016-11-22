38a1a082ec245a619b0bb051edba43f2b3ec42f1
David Solis <david@soliskit.com>
2016-11-22 01:49:32 -0800

    concatenate asset files

---
+++ app/assets/assets_template.rb
@@ -0,0 +1,20 @@
+remove_file "app/assets/javascripts/application.js"
+file 'app/assets/javascripts/application.js', <<-JS
+//= require jquery2
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+JS
+
+remove_file "app/assets/stylesheets/application.css"
+file 'app/assets/stylesheets/application.scss', <<-CSS
+@import
+  'bootstrap-sprockets',
+  'bootstrap',
+  'main';
+CSS
+
+file 'app/assets/stylesheets/main.scss.erb', <<-CSS
+@import "bootstrap/variables";
+CSS
--- app/assets_template.rb
+++
@@ -1,7 +0,0 @@
-copy_file "app/assets/stylesheets/application.scss"
-copy_file "app/assets/stylesheets/main.scss.erb"
-
-remove_file "app/assets/stylesheets/application.css"
-remove_file "app/assets/javascripts/application.js"
-
-copy_file "app/assets/javascripts/application.js"
