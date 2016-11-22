608800fe8c388a9161ca8e383085ce87a964ab4d
David Solis <david@soliskit.com>
2016-11-22 01:50:00 -0800

    add comments to gem file

---
+++ gem_template.rb
@@ -0,0 +1,54 @@
+remove_file "Gemfile"
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+
+ruby '2.3.2'
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails'
+# Use postgresql as the database for Active Record
+gem 'pg'
+# Use Puma as the app server
+gem 'puma'
+# Use Redis adapter to run Action Cable in production
+gem 'redis'
+
+# Use Bootstrap with SCSS
+gem 'bootstrap-sass'
+# Use SCSS for stylesheets
+gem 'sass-rails'
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier'
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails'
+
+# Rails forms made easy
+gem 'simple_form'
+
+# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
+gem 'turbolinks'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder'
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platform: :mri
+  # Testing framework for Rails 5.x
+  gem 'rspec-rails'
+  # Access .env file before starting app
+  gem 'dotenv-rails'
+end
+
+group :development do
+  # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
+  gem 'web-console'
+  gem 'listen'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+  gem 'spring-watcher-listen'
+end
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+RUBY
