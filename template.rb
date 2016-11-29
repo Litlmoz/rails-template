6d8635de61ebafc8da54c1157c80b1da18cf686e
David Solis <david@soliskit.com>
2016-11-29 14:11:04 -0500

    check rails version min

--- template.rb
+++ template.rb
@@ -1,5 +1,7 @@
+RAILS_REQUIREMENT = "~> 5.0.0"
 
 def apply_template!
+  assert_minimum_rails_version
   template "Gemfile.tt", :force => true # define default gems
 
   apply "app/assets/assets_template.rb" # import Bootstrap js and css
@@ -11,6 +13,15 @@ def apply_template!
   template "app.json.tt" # Heroku review app config
 end
 
+def assert_minimum_rails_version
+  requirement = Gem::Requirement.new(RAILS_REQUIREMENT)
+  rails_version = Gem::Version.new(Rails::VERSION::STRING)
+  return if requirement.satisfied_by?(rails_version)
+
+  prompt = "This template requires Rails #{RAILS_REQUIREMENT}. "\
+           "You are using #{rails_version}. Continue anyway?"
+  exit 1 if no?(prompt)
+end
 apply_template!
 
 after_bundle do
