88383b6fc2b4e8cb933d735ced8751300a689b8e
David Solis <david@soliskit.com>
2016-11-29 14:10:10 -0500

    configure dev mailer

--- config/application_template.rb
+++ config/application_template.rb
@@ -15,3 +15,21 @@ application do <<-RUBY
     end
 RUBY
 end
+
+# Configure Dev Mailer
+mailer_regex = /config\.action_mailer\.raise_delivery_errors = false\n/
+
+comment_lines "config/environments/development.rb", mailer_regex
+
+insert_into_file "config/environments/development.rb", :after => mailer_regex do
+  <<-RUBY
+  # Ensure mailer works in dev environment.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {:host => 'localhost:5000'}
+  config.action_mailer.asset_host = "http://localhost:5000"
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:5000'
+  RUBY
+end
