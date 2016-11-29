89a722b23f72d76dd563f98b351fff5a17e2d957
David Solis <david@soliskit.com>
2016-11-29 14:13:12 -0500

    check for template locally

--- template.rb
+++ template.rb
@@ -2,6 +2,8 @@ RAILS_REQUIREMENT = "~> 5.0.0"
 
 def apply_template!
   assert_minimum_rails_version
+  add_template_repository_to_source_path
+
   template "README.md.tt", :force => true # replace default README
   template "Gemfile.tt", :force => true # define default gems
 
@@ -11,8 +13,12 @@ def apply_template!
   apply "config/puma_template.rb" # config puma server setttings
   apply "app/views/layouts/application/_partials.rb" # eg. navbar & footer
   gsub_file "config/cable.yml", /url: .*/, "url: <%= ENV['REDIS_URL'] %>"
+
   template "app.json.tt" # Heroku review app config
 end
+
+require "fileutils"
+require "shellwords"
 
 def assert_minimum_rails_version
   requirement = Gem::Requirement.new(RAILS_REQUIREMENT)
@@ -23,6 +29,25 @@ def assert_minimum_rails_version
            "You are using #{rails_version}. Continue anyway?"
   exit 1 if no?(prompt)
 end
+
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    source_paths.unshift(tempdir = Dir.mktmpdir("rails-template-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git :clone => [
+      "--quiet",
+      "https://github.com/litlmoz/rails-template.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
+
 apply_template!
 
 after_bundle do
