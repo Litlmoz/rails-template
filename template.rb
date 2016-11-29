764252b67fb1bb287673e24820892ba09b429042
David Solis <david@soliskit.com>
2016-11-29 11:31:13 -0500

    add line comments

--- template.rb
+++ template.rb
@@ -9,20 +9,20 @@ def apply_template!
   apply "app/helpers/helper_templates.rb" # date format helper, json serializer
   apply "config/application_template.rb" # config logger, default generators
   apply "config/puma_template.rb" # config puma server setttings
-  apply "lib/scaffold_template.rb"
+  apply "lib/partials.rb" # partial views
   gsub_file "config/cable.yml", /url: .*/, "url: <%= ENV['REDIS_URL'] %>"
 end
 
 apply_template!
 
 after_bundle do
-  generate "simple_form:install"
-  generate "simple_form:install --bootstrap"
-  generate "rspec:install"
+  generate "simple_form:install" # initialize SimpleForm
+  generate "simple_form:install --bootstrap" # initialize SimpleForm Bootstrap
+  generate "rspec:install" # install Rspec
   remove_dir "test" # remove unused test specs
-  remove_dir "lib/templates/erb/scaffold"
-  copy_file "lib/templates/erb/scaffold/_form.html.erb"
-  copy_file "lib/templates/erb/scaffold/index.html.erb"
+  remove_dir "lib/templates/erb/scaffold" # remove default scaffolds
+  copy_file "lib/templates/erb/scaffold/_form.html.erb" # custom form scaffold
+  copy_file "lib/templates/erb/scaffold/index.html.erb" # custom index scaffold
   rails_command "db:create"
   rails_command "db:migrate"
 end
