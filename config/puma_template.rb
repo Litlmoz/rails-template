e548b011259b8b38486b05d0fbca6b2542cd0272
David Solis <david@soliskit.com>
2016-11-29 13:38:06 -0500

    format puma configs

--- config/puma_template.rb
+++ config/puma_template.rb
@@ -1,5 +1,3 @@
-copy_file "Procfile"
-
 remove_file "config/puma.rb"
 
 file "config/puma.rb", <<-RUBY
@@ -7,30 +5,5 @@ plugin :tmp_restart
 plugin :heroku
 RUBY
 
-file "lib/puma/plugin/heroku.rb", <<-RUBY
-Puma::Plugin.create do
-  def config(c)
-
-    threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-    c.threads threads_count, threads_count
-
-    c.preload_app!
-
-    c.port        ENV.fetch("PORT") { 5000 }
-    c.environment ENV.fetch("RAILS_ENV") { "development" }
-
-    if workers_supported?
-      c.workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-      c.before_fork do
-        ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
-      end
-      c.on_worker_boot do
-        ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-      end
-    end
-  end
-
-  VERSION = "1.0.0"
-end
-RUBY
+template "lib/puma/plugin/heroku.rb.tt" # Config Puma server to Heroku defaults
+template "Procfile.tt" # Specify App launch commands
