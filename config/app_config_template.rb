ce8a9210d4a2056241c6ccbc002648e90dd90942
David Solis <david@soliskit.com>
2016-12-05 17:58:16 -0800

    remove controller specs

--- config/app_config_template.rb
+++ config/app_config_template.rb
@@ -10,6 +10,7 @@ application do <<-RUBY
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.view_specs false
+      g.controller_specs false
       g.helper_specs false
       g.stylesheets false
       g.javascripts false
